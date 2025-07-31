79d5b62f916b2f0167f356e102563f7fb7289214
kseniya.durna@gmail.com <kseniya.durna@gmail.com>
2025-07-31 10:00:32 +0000

    finance daily model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,17 +1,17 @@
 select
-    orders.orders_id
-    ,round(
+    orders.orders_id,
+    round(
         orders.margin + ship.shipping_fee - ship.logcost - ship.ship_cost,
         2
-    ) as operational_margin
-    ,orders.date_date
-    ,orders.revenue
-    ,orders.quantity
-    ,orders.purchase_cost
-    ,orders.margin
-    ,ship.shipping_fee
-    ,ship.logcost
-    ,ship.ship_cost,
+    ) as operational_margin,
+    orders.date_date,
+    orders.revenue,
+    orders.quantity,
+    orders.purchase_cost,
+    orders.margin,
+    ship.shipping_fee,
+    ship.logcost,
+    ship.ship_cost
 from
     {{ ref('int_orders_margin') }} as orders
 left join
