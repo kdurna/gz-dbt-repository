20e2cc9cc988b3f1d210f76fc6a2495d1ce57086
kseniya.durna@gmail.com <kseniya.durna@gmail.com>
2025-07-31 14:17:10 +0000

    finance_campaigns_day mart created

---
+++ models/marts/finance/finance_campaigns_day.sql
@@ -0,0 +1,20 @@
+select 
+    campaigns.date_date
+    ,campaigns.paid_source
+    ,finance.tot_operational_margin - campaigns.tot_ads_cost as ads_margin
+    ,finance.average_basket
+    ,campaigns.tot_ads_cost
+    ,campaigns.tot_impression as ads_impressions
+    ,campaigns.tot_clicks as ads_clics
+    ,finance.tot_quantity
+    ,finance.tot_revenue
+    ,finance.tot_purchase_cost
+    ,finance.tot_operational_margin
+    ,finance.tot_shipping_fee
+    ,finance.tot_logcost
+from
+    {{ ref('int_campaigns_day')}} as campaigns
+left join
+    {{ ref('finance_days')}} as finance
+using (date_date)
+order by campaigns.date_date desc, campaigns.paid_source
